497dfc749cd717e33de4c51cab1342ca0e4a7044
JackGlitch101 <135707958+JackGlitch101@users.noreply.github.com>
2024-09-26 14:39:00 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,7 +1,18 @@
-min_length = 5
-user_password = input("Enter your password: ")
-while len(user_password) < min_length:
-    print("Password is too short.")
+def main():
+    min_length = 5
+    user_password = get_password(min_length)
+    print_password(user_password)
+
+
+def print_password(user_password):
+    print(len(user_password) * "*")
+
+
+def get_password(min_length):
     user_password = input("Enter your password: ")
-star_length = len(user_password)
-print(star_length * "*")
+    while len(user_password) < min_length:
+        print("Password is too short.")
+        user_password = input("Enter your password: ")
+    return user_password
+
+main()
