29fdbb0d833b8c32b5f7fd87043e31bd570d4170
JackGlitch101 <135707958+JackGlitch101@users.noreply.github.com>
2024-10-10 14:22:45 +1000

    modify the load_data function to return subject_data and add the display_subject_data function

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,23 +7,24 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = load_data()
-    print(data)
+    subject_data = load_data()
+    display_subject_data(subject_data)
 
 
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    subject_data = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        subject_data.append(parts)
     input_file.close()
+    return subject_data
 
+def display_subject_data(subject_data):
+    for subject in subject_data:
+        print(f"{subject[0]} is taught by {subject[1]:12} and has {subject[2]:3} students")
 
 main()
