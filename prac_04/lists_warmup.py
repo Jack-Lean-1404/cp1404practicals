fe7ef9573b1b0d70b6868caaff1299be805f4f6f
JackGlitch101 <135707958+JackGlitch101@users.noreply.github.com>
2024-10-10 13:29:42 +1000

    complete all warmup questions

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,44 @@
+# Q.1
+# 3
+
+# Q.2
+# 2
+
+# Q.3
+# 1
+
+# Q.4
+# 2
+
+# Q.5
+# 3, 1
+
+# Q.6
+# True
+
+# Q.7
+# False
+
+# Q.8
+# False
+
+# Q.9
+# [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+# Q.1
+# numbers = [3, 1, 4, 1, 5, 9, 2]
+# numbers[0] = "ten"
+# print(numbers)
+
+# Q.2
+# numbers = [3, 1, 4, 1, 5, 9, 2]
+# numbers[6] = 1
+# print(numbers)
+
+# Q.3
+# numbers = [3, 1, 4, 1, 5, 9, 2]
+# print(numbers[2:])
+
+# Q.4
+# numbers = [3, 1, 4, 1, 5, 9, 2]
+# print(9 in numbers)
