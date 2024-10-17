5094fbb218284045f1299358e3f2afdeed61f2af
JackGlitch101 <135707958+JackGlitch101@users.noreply.github.com>
2024-10-17 14:24:08 +1000

    Complete prac 5 Word Occurrences exercise

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,22 @@
+# Guess = 15mins and 30sec
+# Actual = 13mins and 6sec
+
+word_to_occurrence = {}
+
+string = input("Enter a string: ")
+
+words = string.split()
+for word in words:
+    frequency = word_to_occurrence.get(word, 0)
+    word_to_occurrence[word] = frequency + 1
+
+words = list(word_to_occurrence.keys())
+words.sort()
+
+max_length = max((len(word) for word in words))
+for word in words:
+    print(f"{word:{max_length}} : {word_to_occurrence[word]}")
+
+print(word_to_occurrence)
+
+
