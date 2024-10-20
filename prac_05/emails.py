0623918a0a45b65aab17500bb84ff814434821ab
JackGlitch101 <135707958+JackGlitch101@users.noreply.github.com>
2024-10-20 13:09:09 +1000

    Update the code based on feedback and reformat file to meet PEP8

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -2,26 +2,26 @@
 # Actual =
 
 def main():
-    """Create emails_to name dictionary"""
-    email_to_name = {}
+    """Create a dictionary of names and emails"""
+    name_to_email = {}
     email = input("Email: ")
     while email != "":
         name = get_name_from_email(email)
         review = input(f"Is your name {name}? (Y/n) ")
         if review.upper() != "Y" and review != "":
             name = input("Name: ")
-        email_to_name[email] = name
+        name_to_email[email] = name
         email = input("Email: ")
 
-    for email, name in email_to_name.items():
+    for email, name in name_to_email.items():
         print(f"{name} ({email})")
 
+
 def get_name_from_email(email):
-    """Extract name from email"""
+    """Return prefix from email"""
     prefix = email.split('@')[0]
     parts = prefix.split('.')
-    name = " ".join(parts).title()
-    return name
+    return " ".join(parts).title()
+
 
 main()
-
