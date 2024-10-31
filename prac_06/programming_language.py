6ce327872059002042e2b12bb8c0867b6d1cafd0
JackGlitch101 <135707958+JackGlitch101@users.noreply.github.com>
2024-10-31 14:35:49 +1000

    Complete all required tasks for the Intermediate Exercises

---
+++ prac_06/programming_language.py
@@ -0,0 +1,19 @@
+"""Guess = 25mins, Actual = 20"""
+
+class ProgrammingLanguage:
+    """Represent a Programming language object."""
+
+    def __init__(self, language, typing, reflection, year):
+        """Initialise a programming language instance."""
+        self.language = language
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+
+    def __str__(self):
+        """Return language details"""
+        return f"{self.language}, {self.typing}, Reflection = {self.is_dynamic()}, First appeared in {self.year}"
+
+    def is_dynamic(self):
+        return self.typing == "Dynamic"
