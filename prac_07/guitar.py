c73e7aa60ab563bb26ccc1190beba72aaa3a7c27
JackGlitch101 <135707958+JackGlitch101@users.noreply.github.com>
2024-11-07 14:34:58 +1000

    Add required files for the More Guitars! exercise.

---
+++ prac_07/guitar.py
@@ -0,0 +1,26 @@
+"""Guess = 20mins, Actual = 15mins"""
+
+CURRENT_YEAR = 2024
+VINTAGE_AGE = 50
+
+class Guitar:
+    """Stores details about a guitar."""
+
+    def __init__(self, name="", year=0, cost=0):
+        """Initialise a guitar instance."""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+
+    def __str__(self):
+        """Return guitar details."""
+        return f"{self.name} ({self.year}) : {self.cost}"
+
+    def get_age(self):
+        """Determine the age of the guitar."""
+        return CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        """Determine if guitar is vintage based on age."""
+        return self.get_age() >= VINTAGE_AGE
