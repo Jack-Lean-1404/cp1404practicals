698d0a73b8a5da4fbdb99b37df7829ffeeaae19a
JackGlitch101 <135707958+JackGlitch101@users.noreply.github.com>
2024-11-07 14:28:58 +1000

    update language_file_Reader.py and programming_language.py to add the Pointer Arithmetic class

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -7,20 +7,25 @@ Programming Language class with tests.
 class ProgrammingLanguage:
     """Represent information about a programming language."""
 
-    def __init__(self, name, typing, reflection, year):
+    def __init__(self, name, typing, reflection, year, arithmetic_pointer):
         """Construct a ProgrammingLanguage from the given values."""
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
+        self.arithmetic_pointer = arithmetic_pointer
 
     def __repr__(self):
         """Return string representation of a ProgrammingLanguage."""
-        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}, Arithmetic Pointer={self.arithmetic_pointer}"
 
     def is_dynamic(self):
         """Determine if language is dynamically typed."""
         return self.typing == "Dynamic"
+
+    def is_pointer(self):
+        """Determine if language has an arithmetic_pointer."""
+        return self.arithmetic_pointer == "True"
 
 
 def run_tests():
