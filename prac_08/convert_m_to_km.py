239ee8c600a6d4cb0fe77427f13eeb4422cc51c0
JackGlitch101 <135707958+JackGlitch101@users.noreply.github.com>
2024-11-19 12:47:01 +1000

    Add a Constant to the Miles to Kilometres Converter

--- prac_08/convert_m_to_km.py
+++ prac_08/convert_m_to_km.py
@@ -9,31 +9,34 @@ from kivy.lang import Builder
 
 class MilesToKilometresConverter(App):
     """MilesToKilometresConverter is a Kivy App for converting miles to kilometres"""
+
+    FORMULA = 1.609  # Conversion factor: 1 mile = 1.609 km
+
     def build(self):
         """ build the Kivy app from the kv file """
         self.title = "Miles To Kilometres Converter"
         self.root = Builder.load_file('convert_m_to_km.kv')
         return self.root
 
-    # def handle_calculate(self, value):
-    #     """ handle calculation (could be button press or other call), output result to label widget """
-    #     try:
-    #         result = float(value) ** 2
-    #         self.root.ids.output_label.text = str(result)
-    #     except ValueError:
-    #         pass
-
     def increment_miles(self, direction):
         """increment the miles by the given direction"""
-        miles = float(self.root.ids.input_miles.text)
+        miles = self.validate_miles()
         miles += direction
         self.root.ids.input_miles.text = str(miles)
 
     def convert_miles(self):
         """Calculate the conversion from miles to kilometres"""
-        miles = float(self.root.ids.input_miles.text)
-        km = miles * 1.609
+        miles = self.validate_miles()
+        km = miles * self.FORMULA
         self.root.ids.output_label.text = str(km)
+
+    def validate_miles(self):
+        """Validate the miles input"""
+        try:
+            value = float(self.root.ids.input_miles.text)
+            return value
+        except ValueError:
+            return 0
 
 
 MilesToKilometresConverter().run()
