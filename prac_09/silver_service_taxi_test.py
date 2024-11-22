68c8ec18abb343a5d8948df3ca585438990f56ba
JackGlitch101 <135707958+JackGlitch101@users.noreply.github.com>
2024-11-22 12:50:46 +1000

    Complete all sections for the Inheriting Enhancements exercise

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -4,9 +4,9 @@ from prac_09.silver_service_taxi import SilverServiceTaxi
 def main():
     """Test SilverServiceTaxi class"""
     silver_taxi = SilverServiceTaxi("silverbox 1", 100, 4)
-    silver_taxi.drive(30)
+    silver_taxi.drive(38)
     print(silver_taxi)
-    print(silver_taxi.get_fare())
-
+    total_fare = silver_taxi.get_fare()
+    print(f"${total_fare:.2f}")
 
 main()
